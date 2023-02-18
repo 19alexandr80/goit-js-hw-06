12e4fe19a36f5cd134e8830d8ecb2dd61943e8cc
19alexandr80 <usanka1980@gmail.com>
2023-02-18 21:01:31 +0200

    hw#9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const formEl = document.querySelector(".login-form");
+formEl.addEventListener("submit", formLoginCorekt);
 
+function formLoginCorekt(e) {
+  e.preventDefault();
+  const password = e.currentTarget.elements.password.value;
+  const email = e.currentTarget.elements.email.value;
+  const formSet = {
+    email,
+    password,
+  };
+  !password || !email ? alert("Заповніть усі поля") : onSet(formSet);
+}
+function onSet(formSet) {
+  // const formData = new FormData(e.currentTarget);
+  // console.log(formData);
+  console.log(formSet);
+  formEl.reset();
+}
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
+const buttonEl = document.querySelector(".change-color");
+const spanEl = document.querySelector(".color");
+const bodyEl = document.querySelector("body");
+buttonEl.addEventListener("click", getRandomHexColor);
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  const collor = `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+  spanEl.textContent = collor;
+  bodyEl.style.backgroundColor = collor;
+  // return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
