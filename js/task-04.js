b111a1236ddafbfad15f374b589093a673001ee6
19alexandr80 <usanka1980@gmail.com>
2023-02-19 15:52:08 +0200

    switch

--- js/task-04.js
+++ js/task-04.js
@@ -2,10 +2,19 @@ const counterButtonGrupe = document.querySelector("#counter");
 const counterValue = counterButtonGrupe.querySelector("#value");
 counterValue.textContent = 0;
 counterButtonGrupe.addEventListener("click", (e) => {
-  if (e.target.dataset.action === "decrement") {
-    counterValue.textContent = +counterValue.textContent - 1;
-  }
-  if (e.target.dataset.action === "increment") {
-    counterValue.textContent = +counterValue.textContent + 1;
+  // if (e.target.dataset.action === "decrement") {
+  //   counterValue.textContent = +counterValue.textContent - 1;
+  // }
+  // if (e.target.dataset.action === "increment") {
+  //   counterValue.textContent = +counterValue.textContent + 1;
+  // }
+  switch (e.target.dataset.action) {
+    case "decrement":
+      counterValue.textContent = Number(counterValue.textContent) - 1;
+      break;
+
+    case "increment":
+      counterValue.textContent = Number(counterValue.textContent) + 1;
+      break;
   }
 });
