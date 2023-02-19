805890f43098d1f724788270543e8a3fa52c6ecf
19alexandr80 <usanka1980@gmail.com>
2023-02-19 18:42:45 +0200

    __

--- js/task-01.js
+++ js/task-01.js
@@ -3,5 +3,5 @@ console.log("Number of categories: " + categor.children.length);
 const mass = categor.children;
 for (const iterator of mass) {
   console.log("Category: " + iterator.querySelector("h2").outerText);
-  console.log("Elements:" + iterator.querySelectorAll("li").length);
+  console.log("Elements: " + iterator.querySelectorAll("li").length);
 }
